bb7be68e0ac5a9552291e68e5aaeeb02ed3e0d46
100gods <shashank.2022@gmail.com>
2013-12-01 17:30:23 +0530

    Update UtopianTree.java

--- UtopianTree.java
+++ UtopianTree.java
@@ -5,7 +5,11 @@ import java.math.*;
 import java.util.regex.*;
 
 /*
-The Utopian tree goes through 2 cycles of growth every year. The first growth cycle of the tree is during the monsoon season when it doubles in height. The second growth cycle is during the summer when it increases in height by 1 meter. If a new Utopian tree sampling of height 1 meter is planted just before the onset of the monsoon season, can you find the height of the tree after N cycles?
+The Utopian tree goes through 2 cycles of growth every year. 
+The first growth cycle of the tree is during the monsoon season when it doubles in height.
+The second growth cycle is during the summer when it increases in height by 1 meter. 
+If a new Utopian tree sampling of height 1 meter is planted just before the onset of the monsoon season, 
+can you find the height of the tree after N cycles?
 */
 public class Solution {
 
